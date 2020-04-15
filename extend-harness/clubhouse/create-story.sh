0f8de63e0b43379459caa4a1e00f939c057d32c7
Bryan <bfeuling@gmail.com>
2020-04-15 12:30:32 -0500

    Update Secret
    Update Secret

--- extend-harness/clubhouse/create-story.sh
+++ extend-harness/clubhouse/create-story.sh
@@ -1,3 +1,3 @@
 # CREATE CLUBHOUSE STORY
 
-export STORY_ID=`curl -X POST -L "${API_URL}/stories?token=${secrets.getValue("clubhouse-token")}" -H "Content-Type: application/json" -d "{\"comments\": [{ \"text\": \"Deployment URL: ${deploymentUrl}.\n${service.name} deploying to ${env.name}.\nTriggered by: ${deploymentTriggeredBy}\" }], \"description\": \"Artifact: ${artifact.metadata.image}\", \"name\": \"Deploying ${service.name}\", \"project_id\": 2, \"story_type\": \"chore\" }" | jq -r '.id'`
+export STORY_ID=`curl -X POST -L "${API_URL}/stories?token=${SECRET}" -H "Content-Type: application/json" -d "{\"comments\": [{ \"text\": \"Deployment URL: ${deploymentUrl}.\n${service.name} deploying to ${env.name}.\nTriggered by: ${deploymentTriggeredBy}\" }], \"description\": \"Artifact: ${artifact.metadata.image}\", \"name\": \"Deploying ${service.name}\", \"project_id\": 2, \"story_type\": \"chore\" }" | jq -r '.id'`
