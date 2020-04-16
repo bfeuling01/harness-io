6ba436990b18add93535d056ca42ac7c7aef6f04
Bryan <bfeuling@gmail.com>
2020-04-16 09:17:55 -0500

    Updated variables
    Updated variables

--- extend-harness/azure-boards/approve-item.sh
+++ extend-harness/azure-boards/approve-item.sh
@@ -1,11 +1,11 @@
-echo ${Azure_PAT} | az devops login --organization ${azureOrg}
+echo ${secrets.getValue("azure-pat")} | az devops login --organization <azure org>
 
-CURRENT_STATE=`az boards work-item show --id ${wiID} --query 'fields."System.State"'`
+CURRENT_STATE=`az boards work-item show --id ${AZ.AZURE_WI_ID} --query 'fields."System.State"'`
 DESIRED_STATE='"Done"'
 
 while [[ ${CURRENT_STATE} != ${DESIRED_STATE} ]]; do
   echo "Move to Done"
-  CURRENT_STATE=`az boards work-item show --id ${wiID} --query 'fields."System.State"'`
+  CURRENT_STATE=`az boards work-item show --id ${AZ.AZURE_WI_ID} --query 'fields."System.State"'`
   echo "Current State: ${CURRENT_STATE}"
   sleep 5
 done
