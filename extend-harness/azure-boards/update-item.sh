6c49d3b46a7f20837c78655853f18064e65d50cd
Bryan <bfeuling@gmail.com>
2020-04-17 17:09:39 -0500

    Simplified Script
    Simplified Script

--- extend-harness/azure-boards/update-item.sh
+++ extend-harness/azure-boards/update-item.sh
@@ -1,3 +1,1 @@
-echo ${Azure_PAT} | az devops login --organization ${azureOrg}
-
-az boards work-item update --id ${wiID} --discussion "${wiDiscuss}" --state ${wiState}
+az boards work-item update --id ${wiID} --discussion "${wiDiscuss}" --state "${wiState}"
