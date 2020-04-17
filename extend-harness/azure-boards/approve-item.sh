4564e273f5cd555017e002410ac4a4a79dc64c51
Bryan <bfeuling@gmail.com>
2020-04-17 17:10:19 -0500

    Simplified Script
    Simplified Script

--- extend-harness/azure-boards/approve-item.sh
+++ extend-harness/azure-boards/approve-item.sh
@@ -1,4 +1,5 @@
-echo ${secrets.getValue("azure-pat")} | az devops login --organization <azure org>
+export AZURE_DEVOPS_EXT_PAT=${secrets.getValue("azure-pat")}
+az devops configure --defaults organization=https://dev.azure.com/<your org>
 
 CURRENT_STATE=`az boards work-item show --id ${AZ.AZURE_WI_ID} --query 'fields."System.State"'`
 DESIRED_STATE='"Done"'
@@ -9,3 +10,5 @@ while [[ ${CURRENT_STATE} != ${DESIRED_STATE} ]]; do
   echo "Current State: ${CURRENT_STATE}"
   sleep 5
 done
+
+export HARNESS_APPROVAL_STATUS="APPROVED"
