988ba5c7155d86587d5b84cfdfdf63dccc581b76
Bryan <bfeuling@gmail.com>
2020-11-10 17:13:52 -0600

    Created approval script
    Created approval script

---
+++ extend-harness/smartsheet/approval.sh
@@ -0,0 +1,24 @@
+echo "### BEARER TOKEN #####"
+TOKEN=${secrets.getValue("ss_token")}
+
+echo "### SMARTSHEET URL###"
+URL="https://api.smartsheet.com/2.0/sheets"
+
+echo "#### GET SHEET ID #####"
+ID=`curl -X GET ${URL} -H "Authorization: Bearer ${TOKEN}" | jq --arg sheet ${workflow.variables.sheet_name} '.data[] | select(.name == $sheet) | .id'`
+
+echo "#### GET CURRENT STATUS #####"
+CUR_STATUS=`curl -X GET "${URL}/${ID}/rows/${content.ss.ROW_ID}" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" | jq '.cells[0].value'`
+curl -X GET "${URL}/${ID}/rows/${context.ss.ROW_ID}" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" | jq '.cells[0].value'
+DES_STATUS='"Approved"'
+
+echo "#### STATUS TEST #####"
+while [[ ${CUR_STATUS} != ${DES_STATUS} ]]; do
+  echo "Change status to Approved"
+  CUR_STATUS=`curl -X GET "${URL}/${ID}/rows/${context.ss.ROW_ID}" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" | jq '.cells[0].value'`
+  echo ${CUR_STATUS}
+  echo ${DES_STATUS}
+  sleep 5
+done
+
+export HARNESS_APPROVAL_STATUS="APPROVED"
