b7845e3a39d90fca9c88c20372e7314ea8bbbdfc
Bryan <bfeuling@gmail.com>
2020-12-08 14:31:55 -0500

    Delegate Profile Script created
    Delegate Profile Script created

---
+++ extend-harness/gcpSecretManager/delegate-profile.sh
@@ -0,0 +1,28 @@
+apt-get update
+
+# Install Python
+apt-get install python -y
+
+if which gcloud; then
+  echo gcloud already installed
+else
+
+# Install the Google Cloud CLI tool tarball
+  curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-291.0.0-linux-x86_64.tar.gz
+
+#Unzip tarball
+  tar zxvf google-cloud-sdk-291.0.0-linux-x86_64.tar.gz && cd google-cloud-sdk
+
+# Install the CLI tool
+  cat <<EOF | ./install.sh
+n
+y
+EOF
+  export PATH=$PATH:/opt/harness-delegate/google-cloud-sdk/bin
+fi
+
+export PATH=$PATH:/opt/harness-delegate/google-cloud-sdk/bin
+
+gcloud components install beta --quiet
+
+echo ${secrets.getValue("custom_secret_manager")} | base64 -d > /root/gcp_sa.json
