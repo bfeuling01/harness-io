cefed2e64a8041bfe6836da35115146384cb6604
Bryan <16353421+bfeuling01@users.noreply.github.com>
2021-02-17 11:13:43 -0600

    update script
    update script

--- extend-harness/aws/cloudfront/unzip-upload.sh
+++ extend-harness/aws/cloudfront/unzip-upload.sh
@@ -3,6 +3,6 @@ export AWS_SECRET_ACCESS_KEY=${secrets.getValue("<insert secret>")}
 
 unzip /tmp/${service.name}/${artifact.metadata.artifactFileName} -d /tmp/${service.name}/${service.name}
 
-aws s3 cp /tmp/${service.name}/${service.name} s3://${serviceVariable.destination} --recursive
+aws s3 cp /tmp/${service.name}/${service.name} s3://${service.name}/${serviceVariable.destination} --recursive
 
 rm -rf /tmp/${service.name}
