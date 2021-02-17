93124f275ceddadd9137c624bf0493586a5dbd9d
Bryan <16353421+bfeuling01@users.noreply.github.com>
2021-02-17 09:44:41 -0600

    Updated Script
    Updated Script

--- extend-harness/azure/azure-boards/approve-item.sh
+++ extend-harness/azure/azure-boards/approve-item.sh
@@ -1,11 +1,14 @@
-echo ${Azure_PAT} | az devops login --organization ${azureOrg}
+export AZURE_DEVOPS_EXT_PAT=${secrets.getValue("azure-pat")}
+az devops configure --defaults organization=https://dev.azure.com/<your org>
 
-CURRENT_STATE=`az boards work-item show --id ${wiID} --query 'fields."System.State"'`
+CURRENT_STATE=`az boards work-item show --id ${AZ.AZURE_WI_ID} --query 'fields."System.State"'`
 DESIRED_STATE='"Done"'
 
 while [[ ${CURRENT_STATE} != ${DESIRED_STATE} ]]; do
   echo "Move to Done"
-  CURRENT_STATE=`az boards work-item show --id ${wiID} --query 'fields."System.State"'`
+  CURRENT_STATE=`az boards work-item show --id ${AZ.AZURE_WI_ID} --query 'fields."System.State"'`
   echo "Current State: ${CURRENT_STATE}"
   sleep 5
 done
+
+export HARNESS_APPROVAL_STATUS="APPROVED"
