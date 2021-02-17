a802848c4b1f9996f5739b876956748875f810a6
Bryan <16353421+bfeuling01@users.noreply.github.com>
2021-02-17 11:12:23 -0600

    Updated script
    Updated script

--- extend-harness/aws/cloudfront/download-artifact.sh
+++ extend-harness/aws/cloudfront/download-artifact.sh
@@ -4,5 +4,3 @@ export AWS_SECRET_ACCESS_KEY=${secrets.getValue("<insert secret>")}
 mkdir -p /tmp/${service.name}/${service.name}/
 
 aws s3 cp s3://${artifact.metadata.bucketName}/${artifact.metadata.artifactPath} /tmp/${service.name}
-
-ls -la /tmp/${service.name}
