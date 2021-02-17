f9672c3e4b2ff55e7acf10a90e8346fc90fbe185
Bryan <16353421+bfeuling01@users.noreply.github.com>
2021-02-17 09:45:07 -0600

    Updated script
    Updated script

--- extend-harness/azure/azure-boards/create-item.sh
+++ extend-harness/azure/azure-boards/create-item.sh
@@ -1,3 +1,1 @@
-echo ${Azure_PAT} | az devops login --organization ${azureOrg}
-
-az boards work-item create --title "${wiTitle}" --type ${wiType} --area "${wiArea}" --assigned-to ${wiAssignedTo} --description "${wiDescription}" --discussion "${wiDiscuss}" --project ${wiProject} --query id
+export AZURE_WI_ID=`az boards work-item create --title "${wiTitle}" --type "${wiType}" --area "${wiArea}" --assigned-to "${wiAssignedTo}" --description "${wiDescription}" --discussion "${wiDiscuss}" --project "${wiProject}" --query id`
